3c4c10e6f0235f6099675a871c87994b53b751f1
anwinwindfall <anwina@windfallproductions.org>
2024-05-24 10:42:00 +0530

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App({ moduleData }) {
+  // eslint-disable-next-line no-console
+  
+  return (
+    <div className="sw-events-filter__container">
+      
+    </div>
+  );
+}
+
+export default App;
