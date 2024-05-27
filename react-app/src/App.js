c2e239be5476bafc061aed0e4f26e6e1b92edadd
anwinwindfall <anwina@windfallproductions.org>
2024-05-27 15:40:06 +0530

    State and functions

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,13 @@ import './App.scss';
 
 function App({ moduleData }) {
   // eslint-disable-next-line no-console
-  
+  const regionFilterList=[{region_name:"All", region_id:"all"}, ...moduleData.filter_settings.region_filter]
   return (
+    
     <div className="sw-events-filter__container">
       
     </div>
   );
 }
-
+   
 export default App;
--- react-app/src/components/Container.js
+++ react-app/src/components/Container.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Date } from 'core-js'
+import React, { useState } from 'react'
 
-const Container = () => {
-  return (
-    <div>Container</div>
-  )
+const Container = ({ items }) => {
+const [dateCondition, setDateCondition]=useState();
+const [regionFilter, setRegionFilter]=useState("all");
+const currentDate=Date.now();
+const events=[{}];
+function filter(){
+    if (dateCondition=='past') {
+        events=items.filter((item)=>item.event_date<currentDate)
+    }
+    else if(dateCondition=='upcoming'){
+        events=items.filter((item)=>item.event_date>=currentDate)
+    }
+}
+filter();
+function regionalFilter(){
+    const finalData=events.filter((items)=>items.event_region==regionFilter)
+    return finalData
+}
+const finalData=regionalFilter();
+    return (
+        <div>Container</div>
+    )
 }
 
 export default Container
